296e750ba05d44506d798c97e52020604493c296
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2025-08-24 18:02:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "q9JwQQ7T#eR5WB_BtcJjYWke6jd_PKAe_DlzpPUsK-_lHGjcQ4_c",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/ZeroBugZone417/QUEEN-HASUKI-BOT/blob/main/lib/LOGO.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*💥 Bot Status: ONLINE ✅*Hey! I’m your WhatsApp Bot, awake and ready for action.  Type any command or question, and I’ll get back to you instantly! ⚡  Stay tuned for updates, enjoy seamless chatting, and let’s make your experience awesome! 😎 ",
 BOT_OWNER: '94769983151',  // Replace with the owner's phone number
 
 
