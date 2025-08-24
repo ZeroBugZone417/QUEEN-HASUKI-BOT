ba2cdfad22e78afb5658529ffc27b4afbd8c18be
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2025-08-24 17:42:35 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,25 +1,60 @@
-const { cmd, commands } = require('../command');
-const config = require('../config');
+const { cmd } = require("../command");
 
-cmd({
+cmd(
+  {
     pattern: "alive",
-    desc: "Check bot online or no.",
+    react: "🤖",
+    desc: "Show bot status",
     category: "main",
-    filename: __filename
-},
-async (danuwa, mek, m, {
-    from, quoted, body, isCmd, command, args, q, isGroup,
-    sender, senderNumber, botNumber2, botNumber, pushname,
-    isMe, isOwner, groupMetadata, groupName, participants,
-    groupAdmins, isBotAdmins, isAdmins, reply
-}) => {
+    filename: __filename,
+    fromMe: false,
+  },
+  async (malvin, mek, m, { reply }) => {
     try {
-        return await danuwa.sendMessage(from, {
-            image: { url: config.ALIVE_IMG },
-            caption: config.ALIVE_MSG
-        }, { quoted: mek });
+      const from = mek.key.remoteJid;
+
+      await malvin.sendPresenceUpdate("recording", from);
+
+      // Alive Image & Caption
+      await malvin.sendMessage(
+        from,
+        {
+          image: {
+            url: "https://i.ibb.co/SDWZFh23/malvin-xd.jpg",
+          },
+          caption: `𝗠𝗔𝗟𝗨 𝗫𝗗 𝗜𝗦 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪  
+  
+𝗼𝗳𝗳𝗶𝗰𝗶𝗮𝗹 𝘄𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗰𝗵𝗮𝗻𝗲𝗹 -: https://whatsapp.com/channel/0029VbB3YxTDJ6H15SKoBv3S
+
+𝗚𝗶𝘁 𝗛𝘂𝗯 𝗥𝗲𝗽𝗼 -: https://github.com/XdKing2/MALU-XD
+
+𝗢𝘄𝗻𝗲𝗿 -: https://t.me/devmalvin
+          
+*We are not responsible for any*  
+*WhatsApp bans that may occur due to*  
+*the usage of this bot. Use it wisely*  
+*and at your own risk* ⚠️`,
+        },
+        { quoted: mek }
+      );
+
+      // Delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      // Voice Message
+      await malvin.sendMessage(
+        from,
+        {
+          audio: {
+            url: "https://files.catbox.moe/wz8rh7.mp3",
+          },
+          mimetype: "audio/mpeg",
+          ptt: true,
+        },
+        { quoted: mek }
+      );
     } catch (e) {
-        console.log(e);
-        reply(`${e}`);
+      console.error("❌ Error in .alive command:", e);
+      reply("❌ Error while sending alive message!");
     }
-});
+          })
