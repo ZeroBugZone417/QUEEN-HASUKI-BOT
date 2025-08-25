1b24a77127f8e077f2b52e9811f733d2d26f3266
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2025-08-25 10:16:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "WxpwFSjB#PI61VPAAwUiT6oydk8De7by_v4GFwOasZPQUcW9tZos",
+SESSION_ID: process.env.SESSION_ID || "20wG1DRQ#sKYWVqnVC5Hjsmr2obOrqBx83akRaWktzVgxsrlDM_I",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/ZeroBugZone417/QUEEN-HASUKI-BOT/blob/main/lib/LOGO.png?raw=true",
 BOT_OWNER: '94769983151',  // Replace with the owner's phone number
 
