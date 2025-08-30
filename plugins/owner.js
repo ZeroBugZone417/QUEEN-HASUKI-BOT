8275af9a07d6bbf7503c8a07cd44ffd92fc9df3b
ZeroBugZone417 <dinethsudarshana417@gmail.com>
2025-08-30 19:18:49 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,6 +1,5 @@
 const { cmd } = require("../command");
 
-// ===== OWNER COMMAND WITH IMAGE, BUTTONS & vCard =====
 cmd(
   {
     pattern: "owner",
@@ -13,22 +12,23 @@ cmd(
     try {
       // Owner info text
       const ownerInfo = `
-👑 *Bot Owner Info*
-📝 Name: Dineth Sudarshana
+╔═•ೋ° ✧ BOT OWNER ✧ °ೋ•═╗
+👤 Name: Dineth Sudarshana
 📱 WhatsApp: +94769983151
 💻 Role: Developer & Maintainer
 🌐 GitHub: https://github.com/ZeroBugZone417
 📸 Instagram: https://instagram.com/DinethSudarshana
 ✨ Bot Version: 1.0
+╚═════════════════════╝
 `;
 
-      // Buttons
-      const buttons = [
-        { buttonId: ".owner", buttonText: { displayText: "📇 Contact" }, type: 1 },
-        { buttonId: ".bc", buttonText: { displayText: "📢 Broadcast" }, type: 1 },
-      ];
+      // Send image + caption
+      await hasuki.sendMessage(from, {
+        image: { url: "https://i.imgur.com/yourImage.jpg" }, // replace with your image URL
+        caption: ownerInfo,
+      }, { quoted: mek });
 
-      // Send vCard contact
+      // Send vCard contact separately
       const vcard = `
 BEGIN:VCARD
 VERSION:3.0
@@ -37,13 +37,8 @@ TEL;waid=94769983151:+94769983151
 END:VCARD
 `;
 
-      // Send message with image, caption, buttons & contact
       await hasuki.sendMessage(from, {
-        image: { url: "https://i.imgur.com/yourImage.jpg" }, // replace with owner image URL
-        caption: ownerInfo,
-        buttons,
-        headerType: 4,
-        contacts: { displayName: "Dineth Sudarshana", contacts: [{ vcard }] },
+        contacts: { displayName: "Dineth Sudarshana", contacts: [{ vcard }] }
       }, { quoted: mek });
 
     } catch (e) {
